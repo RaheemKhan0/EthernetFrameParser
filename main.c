c03a070e83b1670db07e19d8e683882342d3f7b2
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2026-07-10 13:56:32 +0100

    changed ethernet file name and also added the implementation of the main file

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
+#include "ethernet.h"
+#include "tap.h"
+#include <stdint.h>
 #include <stdio.h>
 
-
 int main() {
-    int x;
-    x = 10 * 10;
-    printf("hello world %d\n", x);
-    return 0;
+
+  int fd = tap_open("tap0");
+  if (fd < 0) {
+    return 1;
+  }
+  printf("TAP device opened, listening...\n");
+  uint8_t buffer[1514];
+
+  while (1) {
+    int n = tap_read(fd, buffer, sizeof(buffer));
+    if (n < 0)
+      break;
+    if (n < 14)
+      continue;
+    parse_ethernet_frame(buffer, n);
+  }
+  tap_close(fd);
+  return 0;
 }
