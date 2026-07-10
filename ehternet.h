2b7db33b2b544625440836b2235f699e51dafeda
Muhammad Raheem Khan <muhammadraheemk96@gmail.com>
2026-07-10 13:38:34 +0100

    added ethernet file to parse frames

---
+++ ehternet.h
@@ -0,0 +1,7 @@
+#ifndef ETHERNET_H
+#define ETHERNET_H
+#include <stdint.h>
+
+void parse_ethernet_frame(const uint8_t *frame, int len);
+
+#endif
---
+++ ethernet.c
@@ -0,0 +1,23 @@
+#include "ehternet.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+struct ethernet_header {
+  uint8_t dest_mac[6];
+  int8_t src_mac[6];
+  uint16_t ethertype;
+} __attribute__((packed));
+
+void parse_ethernet_frame(const uint8_t *frame, int len) {
+  struct ethernet_header *hdr = (struct ethernet_header *)frame;
+
+  uint16_t type = ntohs(hdr->ethertype);
+  printf("Dest MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", hdr->dest_mac[0],
+         hdr->dest_mac[1], hdr->dest_mac[2], hdr->dest_mac[3], hdr->dest_mac[4],
+         hdr->dest_mac[5]);
+
+  printf("Dest MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", hdr->src_mac[0],
+         hdr->src_mac[1], hdr->src_mac[2], hdr->src_mac[3], hdr->src_mac[4],
+         hdr->src_mac[5]);
+  printf("EtherType: 0x%04x\n", type);
+}
